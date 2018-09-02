59c55f24ac83e49cdc23bc357b7ba47c4289ca70
Masud Mohammed <0pixelz0@gmail.com>
2018-09-02 12:23:29 +0100

    predict some data

--- machine-learning/sentdex-youtube/[ep25] SVMFromScratch.py
+++ machine-learning/sentdex-youtube/[ep25] SVMFromScratch.py
@@ -147,5 +147,20 @@ data_dict = {-1: np.array([[1, 7], [2, 8], [3, 8]]), 1: np.array([[5, 1], [6, -1
 
 svm = Support_Vector_Machine()
 svm.fit(data = data_dict)
+
+predict_us = [
+    [0 ,10], 
+    [1, 3],
+    [3, 4],
+    [3, 5],
+    [5, 5],
+    [5, 6],
+    [6, -5],
+    [5, 8]
+]
+
+for p in predict_us:
+    svm.predict(p)
+
 svm.visualise()
 
