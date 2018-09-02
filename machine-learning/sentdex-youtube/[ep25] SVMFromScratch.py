1310dd7762af2a810d6fd11f62ee23b81673c9d1
Masud Mohammed <0pixelz0@gmail.com>
2018-09-02 12:18:46 +0100

    working svm and visualising

--- machine-learning/sentdex-youtube/[ep25] SVMFromScratch.py
+++ machine-learning/sentdex-youtube/[ep25] SVMFromScratch.py
@@ -52,9 +52,8 @@ class Support_Vector_Machine:
         step_sizes = [self.max_feature_value * 0.1, self.max_feature_value * 0.01, self.max_feature_value * 0.001]
 
         #the step range of b
+        #we do not need as fine steps with b
         b_range_multiple = 5
-
-        #
         b_multiple = 5
 
         #
@@ -64,18 +63,89 @@ class Support_Vector_Machine:
             w = np.array([latest_optimum, latest_optimum])
             optimised = False
             while not optimised:
-                pass
+                #we want min w and max b
+                for b in np.arange(-1 * self.max_feature_value * b_range_multiple, self.max_feature_value*b_range_multiple, step*b_multiple):
+                    for transformation in transforms:
+                        #transform w with each transformation
+                        w_t = w*transformation
+                        found_option = True
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi*(np.dot(w_t, xi) + b) >= 1:
+                                    found_option = False
+
+                    if found_option:
+                        #linalg is magnitude of vector
+                        opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+
+                if w[0] < 0:
+                    optimised = True
+                    print('Optimised a step.')
+                else:
+                    w = w - step
+
+            #get smallest magnitude by sorting
+            norms = sorted([n for n in opt_dict])
+
+            #get the w and b from the smallest magnitude (which is key remember)
+            opt_choice = opt_dict[norms[0]]
+
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            
+            #use the smaller step value
+            latest_optimum = opt_choice[0][0]+step*2
 
     #predict new data
     def predict(self, features):
         #the basic algorithm
         #run x.w + b and find out if + or -
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+
+        if classification != 0 and self.visualisation:
+            self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
+
         return classification
 
+    def visualise(self):
+        [[self.ax.scatter(x[0], x[1], s=100, c=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        #v = x.w + b
+        # +ve, v = 1    -ve, v =-1  decision boundary, v = 0
+        def hyperplane(x, w, b ,v):
+            return (-w[0]*x-b+v) / w[1]
+
+        datarange = (self.min_feature_value*0.9, self.max_feature_value * 1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        #+ve hyperplane
+        #these are y points
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2], 'k')
+
+        #-ve hyperplane
+        #these are y points
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2], 'k')
+
+        #decision boundary hyperplane
+        #these are y points
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2], 'y--')
+
+        plt.show()
 
 
 #create a demo dataset to train with
 #two classes: -1, 1
 data_dict = {-1: np.array([[1, 7], [2, 8], [3, 8]]), 1: np.array([[5, 1], [6, -1], [7, 3]])}
+
+svm = Support_Vector_Machine()
+svm.fit(data = data_dict)
+svm.visualise()
 
